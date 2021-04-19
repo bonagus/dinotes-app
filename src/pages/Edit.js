a105b333c03de8f197334e1e12b4b4c817a29f3e
Eko Agus Darmawan <easdarm@gmail.com>
2021-04-19 08:20:29 +0700

    framework 1.0

---
+++ src/pages/Edit.js
@@ -0,0 +1,81 @@
+import React from "react"; 
+import styled from 'styled-components';
+import PageLayout from '../layouts/PageLayout';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 1rem;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  min-width: 30vw;
+  margin: 1rem;
+`;
+
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Label = styled.label`
+  text-align: left;
+  color: #4a5568;
+`;
+
+const Input = styled.input`
+  margin: 1rem 0;
+  padding: 0.5rem;
+`;
+
+const TextArea = styled.textarea`
+  margin: 1rem 0;
+  padding: 0.5rem;
+  resize: none;
+`;
+
+const Button = styled.button`
+  background: #3182ce;
+  color: white;
+  font-size: 1em;
+  margin: 1rem 0;
+  padding: 0.75rem;
+  border: 2px solid white;
+  border-radius: 5px;
+`;
+
+const EditNoteForm = () => {
+    return (
+        <Form>
+            <FormGroup>
+                <Label>Title</Label>
+                <Input type="text" name="title" />
+            </FormGroup>
+            <FormGroup>
+                <Label>Note</Label>
+                <TextArea name="note" rows="12" />
+            </FormGroup>
+            <FormGroup>
+                <Button type="submit">Add</Button>
+                <Button>Delete</Button>
+            </FormGroup>
+        </Form>
+    );
+};
+
+const EditPage = () => {
+    return (
+        <PageLayout>
+            <Container>
+                <h1>Edit Note</h1>
+                <EditNoteForm />
+            </Container>
+        </PageLayout>
+    );
+};
+
+export default EditPage;
